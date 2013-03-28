f7e34b78fc2415d07c856d14cbd0434d886ba061
Christian Schmidt <chrisch@web.de>
2013-03-28 13:37:44 +0100

    id-spalte raus, alle anderen props von Mortgage jetzt auch im grid

--- app/view/MortgageList.js
+++ app/view/MortgageList.js
@@ -15,17 +15,41 @@ Ext.define('MortgageApp.view.MortgageList', {
     title: 'Mortgage List',
     
     columns: [
-              {
+              /*{
                   text: 'ID',
                   width: 100,
                   sortable: false,
                   hideable: false,
                   dataIndex: 'id'
-              },
+              },*/
               {
                   text: 'Name',
-                  width: 150,
+                  // width: 150,
                   dataIndex: 'name',
+                  hidden: false
+              },
+              {
+                  text: 'Price',
+                  // width: 150,
+                  dataIndex: 'price',
+                  hidden: false
+              },
+              {
+                  text: 'Down',
+                  // width: 150,
+                  dataIndex: 'down',
+                  hidden: false
+              },
+              {
+                  text: 'Interest',
+                  // width: 150,
+                  dataIndex: 'interest',
+                  hidden: false
+              },
+              {
+                  text: 'Term',
+                  // width: 150,
+                  dataIndex: 'term',
                   hidden: false
               }
        ],
