7dff8d4b61577de21c72f5e4e64666df12d2a46e
olli <oliver.zeigermann@gmail.com>
2013-03-28 12:35:19 +0100

    Standalone mortgage support

--- app/model/Mortgage.js
+++ app/model/Mortgage.js
@@ -3,18 +3,16 @@ Ext.define('MortgageApp.model.Mortgage', {
 
     fields: [
         { name: 'id', type: 'int' },
-        { name: 'customerId', type: 'int' },
         { name: 'name', type: 'string' },
         { name: 'price', type: 'int' },
         { name: 'down', type: 'int' },
         { name: 'interest', type: 'double' },
         { name: 'term', type: 'int' }
     ],
-    belongsTo: { model: 'MortgageApp.model.Customer', name: 'customer', foreignKey: 'customerId'},
     proxy: {
         type: 'rest',
-//        url : 'http://127.0.0.1:8888/customer/mortgage',
-        url : 'http://mortgage-demo.appspot.com/customer/mortgage',
+//        url : 'http://127.0.0.1:8888/mortgage',
+        url : 'http://mortgage-demo.appspot.com/mortgage',
         reader: {
             type: 'json',
             root: 'payload'
